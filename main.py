aa85c604dd6713bfa970237199e9dad22d72d859
justmelul <epatrol100@gmail.com>
2022-03-21 11:46:54 +0300

    ver 2
    тут я дорабатываю бота, добавляю кнопки, комментирую и решаю ошибки

--- main.py
+++ main.py
@@ -2,87 +2,117 @@ import telebot
 import config
 from telebot import types
 
+# беру токен бота из файла config
 bot = telebot.TeleBot(config.token)
 
 
+# таблица id - таблица id книги id user название книги
+
+# при команде /start вызывается эта часть кода
 @bot.message_handler(commands=['start'])
 def welcome(message):
+    import sqlite3
+    con = sqlite3.connect("database.sqlite")
+    cur = con.cursor()
+
+    # если нет записи  о пользователе в бд, создаем запись
+    if not cur.execute("""SELECT id FROM name WHERE id = ?""", (message.from_user.id,)).fetchall():
+        # создаем запись в бд с новым ид
+        cur.execute("""INSERT INTO name(id) VALUES(?)""", (message.from_user.id,))
+
+    con.commit()
+    cur.close()
+
+    # создаем кнопки для управления
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("список 'мои желания'")
     item2 = types.KeyboardButton("добавить книгу в 'мои желания' (/add)")
     item3 = types.KeyboardButton("удалить книгу из 'мои желания' (/del)")
+    item4 = types.KeyboardButton("удалить все записи из 'мои желания' (или /all)")
 
-    markup.add(item1, item2, item3)
+    # добавляем кнопки
+    markup.add(item1, item2, item3, item4)
 
+    # приветственное сообщение
     bot.send_message(message.chat.id,
                      "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот книжного магазина в котором можно редактировать список 'мои желания'.".format(
                          message.from_user, bot.get_me()),
                      parse_mode='html', reply_markup=markup)
+    #
 
 
+# при команде /add вызывается эта часть кода
 @bot.message_handler(commands=['add'])
-def addtofav(message):
+def add_to_fav(message):
+    # title -- текст написаный поьзователем посе /add
     title = ' '.join(str(message.text).split()[1:])
 
     import sqlite3
     con = sqlite3.connect("database.sqlite")
     cur = con.cursor()
+
+    # for нужен для того что-бы понять, есть-ли записи о пользователе в бд
     for i in cur.execute("""SELECT favorites FROM name WHERE id = ?""", (message.from_user.id,)):
-        if str(i[0]) != '':
+        # если запись есть
+        con = sqlite3.connect("database.sqlite")
+        cur = con.cursor()
+
+        if str(i[0]) != 'None':
             viv = str(i[0]) + f';{title}'
             bot.send_message(message.chat.id, ' | '.join(viv.split(';')))
+            cur.execute("""UPDATE name SET favorites = ? WHERE id = ?""", (viv, message.from_user.id,))
+
+        # если записи о пользователе нет
         else:
             viv = f'{title}'
             bot.send_message(message.chat.id, ' | '.join(viv.split(';')))
-    con.close()
+            cur.execute("""UPDATE name SET favorites = ? WHERE id = ?""", (viv, message.from_user.id,))
 
-    con = sqlite3.connect("database.sqlite")
-    cur = con.cursor()
-    cur.execute("""UPDATE name SET favorites = ? WHERE id = ?""", (viv, message.from_user.id,))
-    con.commit()
-    cur.close()
+        con.commit()
+        cur.close()
 
 
+# при команде /del вызывается эта часть кода
 @bot.message_handler(commands=['del'])
-def delfromfav(message):
+def del_from_fav(message):
     title = ' '.join(str(message.text).split()[1:])
 
     import sqlite3
     con = sqlite3.connect("database.sqlite")
     cur = con.cursor()
     for i in cur.execute("""SELECT favorites FROM name WHERE id = ?""", (message.from_user.id,)):
-        print(i[0])
         viv = i[0].split(';')
+        con = sqlite3.connect("database.sqlite")
+        cur = con.cursor()
+
         if len(viv) == 1:
-            con = sqlite3.connect("database.sqlite")
-            cur = con.cursor()
             viv.clear()
             bot.send_message(message.chat.id, 'список пуст')
-            con.close()
-
-            con = sqlite3.connect("database.sqlite")
-            cur = con.cursor()
-            cur.execute("""UPDATE name SET favorites = ? WHERE id = ?""", ('', message.from_user.id,))
-
-            con.commit()
-            cur.close()
+            cur.execute("""UPDATE name SET favorites = ? WHERE id = ?""", (None, message.from_user.id,))
 
         else:
             del viv[viv.index(title)]
             viv = ';'.join(viv)
             bot.send_message(message.chat.id, ' | '.join(viv.split(';')))
-            con.close()
-
-            con = sqlite3.connect("database.sqlite")
-            cur = con.cursor()
             cur.execute("""UPDATE name SET favorites = ? WHERE id = ?""", (viv, message.from_user.id,))
 
-            con.commit()
-            cur.close()
+        con.commit()
+        cur.close()
+
+
+@bot.message_handler(commands=['all'])
+def del_all(message_):
+    import sqlite3
+    con = sqlite3.connect("database.sqlite")
+    cur = con.cursor()
+    cur.execute("""UPDATE name SET favorites = ? WHERE id = ?""", (None, message_.from_user.id,))
+    bot.send_message(message_.chat.id, 'список пуст')
+    con.commit()
+    cur.close()
 
 
 @bot.message_handler(content_types=['text'])
-def lalala(message):
+def when_text(message):
     if message.chat.type == 'private':
         if message.text == "список 'мои желания'":
 
@@ -90,7 +120,7 @@ def lalala(message):
             con = sqlite3.connect("database.sqlite")
             cur = con.cursor()
             for i in cur.execute("""SELECT favorites FROM name WHERE id = ?""", (message.from_user.id,)):
-                if i[0] == '':
+                if i[0] == '' or str(i[0]) == 'None':
                     bot.send_message(message.chat.id, 'список пуст')
                 else:
                     bot.send_message(message.chat.id, ' | '.join(str(i[0]).split(';')))
@@ -101,7 +131,10 @@ def lalala(message):
                              'что-бы добавить книгу в мои желания использйте команду "/add (введите название книги)"')
         elif message.text == "удалить книгу из 'мои желания' (/del)":
             bot.send_message(message.chat.id,
-                             'что-бы удаить книгу из мои желания использйте команду "/del (введите название книги)"')
+                             'что-бы удалить книгу из мои желания использйте команду "/del (введите название книги)"')
+
+        elif message.text == "удалить все записи из 'мои желания' (или /all)":
+            bot.send_message(message.chat.id, 'что-бы удалить все книги из "мои желания" использйте команду "/all"')
         else:
             bot.send_message(message.chat.id, 'Я не знаю что ответить')
 
